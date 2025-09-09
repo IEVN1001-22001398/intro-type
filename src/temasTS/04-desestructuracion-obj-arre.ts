b850b4611e35610b0ec96dbde936bf3cfe3a37f5
IEVN1001-22001398 <81186@alumnos.utleon.edu.mx>
2025-09-08 21:48:03 -0600

    Desestructuracion de funciones

--- src/temasTS/04-desestructuracion-obj-arre.ts
+++ src/temasTS/04-desestructuracion-obj-arre.ts
@@ -21,13 +21,25 @@ const reproductor:Reproductor={
     }
 };
 
-console.log("el volumen actual es :", reproductor.volumen)
+console.log("El volumen actual es :", reproductor.volumen);
+console.log("El segundo actual es :", reproductor.segundo);
+console.log("El canción actual es :", reproductor.cancion);
+console.log("El autor es :", reproductor.detalles.autor);
 
 const{volumen,segundo,cancion,detalles}=reproductor;
 const{autor:nuevoauto, anio}=detalles;
 
-console.log("el volumen actual es :", volumen)
+console.log("El volumen actual es :", volumen);
+console.log("El segundo actual es :", segundo);
+console.log("El canción actual es :", cancion);
+console.log("El autor es :", nuevoauto);
+console.log("El año es :", anio);
 
-
-
-
+const dbz:string[]=["Goku", "Vegeta", "Trunks"];
+const [p1,p2,p3]=dbz;
+console.log("Personaje 1: ", p1);
+console.log("Personaje 2: ", p2);
+console.log("Personaje 3: ", p3);
+ 
+const[,,p4]=dbz;
+console.log("Personaje 4: ", p4);
